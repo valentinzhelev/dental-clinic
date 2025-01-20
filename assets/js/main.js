6da27e87cda0ff04bbd9a2be52c2b3c0abe09413
ᴠᴀʟᴇɴᴛɪɴ ᴢʜᴇʟᴇᴠ <valentin.jelev@abv.bg>
2025-01-20 13:40:38 +0200

    Added all sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     window.addEventListener("scroll", function () {
         if (window.scrollY > lastScrollY) {
-            // Скрий top-header и премести main-header нагоре
             topHeader.classList.add("hidden");
             mainHeader.classList.add("scrolled");
         } else {
-            // Покажи top-header и върни main-header на мястото му
             topHeader.classList.remove("hidden");
             mainHeader.classList.remove("scrolled");
         }
